91ca6a4ec872ec4b9ec71ad9d009aad884e57e98
Justine <>
2018-03-05 12:36:49 +0100

    added show page for user and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   root 'home#index'
-  get 'users/:id', to: "users/registrations#show"
+  get 'users/:id', to: "users#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
