ca77ac0e395a87727b71d51d1cdd0ca78a75a286
Justine <>
2018-03-06 17:29:22 +0100

    new try but did not work

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,10 @@
 class CartsController < ApplicationController
 
   def show
+    @total = 0
+    current_user.cart.items.each do |item|
+      @total += item.price
+    end 
   end
 
   def add_to_cart
@@ -8,12 +12,9 @@ class CartsController < ApplicationController
       current_user.cart.items << Item.find(params[:id])
     else
       Cart.create(user: current_user)
+      current_user.cart.items << Item.find(params[:id])
     end
     redirect_to cart_path(current_user.cart)
   end
 
-  private
-    def carts_params
-      params.permit(:user, :items)
-    end
 end
