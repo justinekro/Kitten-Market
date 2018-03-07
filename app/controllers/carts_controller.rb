85fbdb8a279fd38f115aaf65a392867ff4506b87
Justine <>
2018-03-07 17:47:31 +0100

    cart finally working git add .git add .

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,20 +1,26 @@
 class CartsController < ApplicationController
 
   def show
-    current_user.cart
     @total = 0
     current_user.cart.items.each do |item|
-      @total += item.price
+      @total += item.price * item.quantity
     end 
   end
 
   def add_to_cart
+    item = Item.find(params[:id])
+    item.quantity +=1
+    item.save
     if user_signed_in?
       if current_user.cart
-        current_user.cart.items << Item.find(params[:id])
+        #current_user.cart.items << Item.find(params[:id])
+          unless current_user.cart.items.include?(item)
+            current_user.cart.items << item  
+          end
       else
         Cart.create(user: current_user)
-        current_user.cart.items << Item.find(params[:id])
+        #current_user.cart.items << Item.find(params[:id])
+        current_user.cart.items << item 
       end
       redirect_to cart_path(current_user.cart)
     else 
@@ -25,7 +31,14 @@ class CartsController < ApplicationController
 
   def remove_from_cart
     item = Item.find(params[:id])
-    current_user.cart.items -= [item]
+    if item.quantity <= 1
+      current_user.cart.items.delete(item)
+      item.quantity = 0
+      item.save
+    else 
+      item.quantity -= 1
+      item.save
+    end
     redirect_to cart_path(current_user.cart)
   end
 
