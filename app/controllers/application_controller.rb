d1ebc2971fab84554f9e37fd8b1b0855333c4d54
Justine <>
2018-03-08 17:22:29 +0100

    try on devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  def configure_devise_parameters
-  	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :firstname, :name, :birthdate, :address, :postal_code, :city, :country)}     
-  	devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :firstname, :name, :birthdate, :address, :postal_code, :city, :country, :current_password)}   
+	before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up).push(:email, :password, :password_confirmation, :firstname, :name, :birthdate, :address, :postal_code, :city, :country)
+  	devise_parameter_sanitizer.for(:account_update).push(:email, :password, :password_confirmation, :firstname, :name, :birthdate, :address, :postal_code, :city, :country, :current_password)
   end
 end
