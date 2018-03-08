8e1e3ab0efe4b6cce039457c06cb41bff52bf1ad
Justine <>
2018-03-08 21:14:16 +0100

    try to change db

--- db/migrate/20180308084343_create_products.rb
+++ db/migrate/20180308084343_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
-      t.integer :quantity
+      t.string :quantity
       t.references :item, foreign_key: true
 
       t.timestamps
