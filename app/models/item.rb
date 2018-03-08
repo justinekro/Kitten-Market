d1fac180e99d28ca044ffa4d6e4859c8848b5e14
Justine <>
2018-03-08 23:58:49 +0100

    modified image size for paperclip

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
 	  has_attached_file :item_image, styles: {
     thumb: '100x100>',
     square: '200x200#',
-    medium: '300x300>',
+    medium: '223x129>',
   }
 
 # Validate the attached image is image/jpg, image/png, etc
